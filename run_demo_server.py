14dde48c86a127676e179addeb80fa7e9e2d037d
Damian Kalinowski <damian.kalinowski@intel.com>
2021-03-12 10:24:04 +0000

    fix

--- run_demo_server.py
+++ run_demo_server.py
@@ -13,6 +13,13 @@ import functools
 import logging
 import collections
 
+import tensorflow as tf
+import model
+from icdar import restore_rectangle
+import lanms
+from eval import resize_image, sort_poly, detect
+import io
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -35,27 +42,18 @@ def get_host_info():
 @functools.lru_cache(maxsize=100)
 def get_predictor(checkpoint_path):
     logger.info('loading model')
-    import tensorflow as tf
-    import model
-    from icdar import restore_rectangle
-    import lanms
-    from eval import resize_image, sort_poly, detect
-
     input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
     global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
 
     f_score, f_geometry = model.model(input_images, is_training=False)
-
     variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
     saver = tf.train.Saver(variable_averages.variables_to_restore())
 
     sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-
     ckpt_state = tf.train.get_checkpoint_state(checkpoint_path)
     model_path = os.path.join(checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
     logger.info('Restore from {}'.format(model_path))
     saver.restore(sess, model_path)
-
     def predictor(img):
         """
         :return: {
@@ -197,12 +195,14 @@ checkpoint_path = './east_icdar2015_resnet_v1_50_rbox'
 @app.route('/', methods=['POST'])
 def index_post():
     global predictor
-    import io
     bio = io.BytesIO()
-    request.files['image'].save(bio)
-    img = cv2.imdecode(np.frombuffer(bio.getvalue(), dtype='uint8'), 1)
+    i = request.files['image']
+    #i.save(bio)
+    i.save('/tmp/file.png')
+    #request.files['image'].save(bio)
+    #img = cv2.imdecode(np.frombuffer(bio.getvalue(), dtype='uint8'), 1)
+    img = cv2.imread('/tmp/file.png').astype(np.uint8)
     rst = get_predictor(checkpoint_path)(img)
-
     save_result(img, rst)
     return render_template('index.html', session_id=rst['session_id'])
 
@@ -220,7 +220,7 @@ def main():
             'Checkpoint `{}` not found'.format(args.checkpoint_path))
 
     app.debug = False  # change this to True if you want to debug
-    app.run('0.0.0.0', args.port)
+    app.run('0.0.0.0', args.port, debug=True)
 
 if __name__ == '__main__':
     main()
